61f070fda64b2f58ac1df0e4ae4efb0a6436ce1d
s.borodanev <boss@curs.ru>
2018-05-22 11:29:53 +0000

    AuthServerAuthenticationProvider
    git-svn-id: https://share.curs.ru/svn/showcase_maven/trunk@135 d262380e-cab2-e44f-bc11-c7317f1b6a35

--- src/main/java/ru/curs/showcase/security/AuthServerAuthenticationProvider.java
+++ src/main/java/ru/curs/showcase/security/AuthServerAuthenticationProvider.java
@@ -63,8 +63,18 @@ public class AuthServerAuthenticationProvider implements AuthenticationProvider 
 		String url = "";
 		String login = arg1.getPrincipal().toString();
 		String pwd = arg1.getCredentials().toString();
+
 		String sesid = ((UserAndSessionDetails) arg1.getDetails()).getSessionId();
-		String oldSesid = AppInfoSingleton.getAppInfo().getSesid();
+		// String oldSesid = AppInfoSingleton.getAppInfo().getSesid();
+		String[] arr =
+			AppInfoSingleton.getAppInfo().getRemoteAddrSessionMap().values()
+					.toArray(new String[0]);
+		String oldSesid = arr.length > 0 ? arr[arr.length - 1] : "";
+		if (sesid == null && "".equals(oldSesid)) {
+			sesid = "RestfulSessId";
+			oldSesid = "RestfulSessId";
+		}
+
 		String groupProviders =
 			((UserAndSessionDetails) arg1.getDetails()).getUserInfo().getGroupProviders();
 
