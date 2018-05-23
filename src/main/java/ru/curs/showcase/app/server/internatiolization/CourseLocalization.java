7ee22b449e67db7e3145740afa23d624d804ea41
s.borodanev <boss@curs.ru>
2018-05-23 09:12:30 +0000

    CourseLocalization
    git-svn-id: https://share.curs.ru/svn/showcase_maven/trunk@143 d262380e-cab2-e44f-bc11-c7317f1b6a35

--- src/main/java/ru/curs/showcase/app/server/internatiolization/CourseLocalization.java
+++ src/main/java/ru/curs/showcase/app/server/internatiolization/CourseLocalization.java
@@ -1,309 +1,280 @@
-package ru.curs.showcase.security;
+package ru.curs.showcase.app.server.internatiolization;
+
+import gnu.gettext.GettextResource;
 
 import java.io.*;
-import java.net.*;
-import java.util.IllegalFormatException;
+import java.util.*;
 
-import javax.net.ssl.*;
-
-import org.slf4j.*;
-import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import ru.curs.showcase.runtime.*;
-import ru.curs.showcase.util.*;
-import ru.curs.showcase.util.exception.SettingsFileOpenException;
 
 /**
- * @author anlug
  * 
- *         Класс, реализующий провайдер аутентификации с помощью AuthServer.
- * 
+ * @author s.borodanev
+ *
+ *         Класс, предназначенный для вызова функциональных возможностей пакета
+ *         GNU GetText. Каждый из методов данного класса перевызывает
+ *         соответствующий метод указанного пакета. Указанный пакет выполняет
+ *         функции локализации и интернационализации.
  */
-public class AuthServerAuthenticationProvider implements AuthenticationProvider {
 
-	static {
-		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-			public boolean verify(String hostname, SSLSession session) {
-				// ip address of the service URL(like.23.28.244.244)
-				// if (hostname.equals("23.28.244.244"))
-				return true;
-				// return false;
-			}
-		});
+public class CourseLocalization {
+
+	/**
+	 * Возвращает перевод переменной <VAR>msgid</VAR>.
+	 * 
+	 * @param catalog
+	 *            - экземпляр класса ResourceBundle
+	 * @param msgid
+	 *            - строка-ключ, которая должна быть переведена, используется
+	 *            ASCII-кодировка
+	 * @return перевод переменной <VAR>msgid</VAR>, или сама переменная
+	 *         <VAR>msgid</VAR>, если перевод не найден
+	 */
+	public static String gettext(ResourceBundle catalog, String msgid) {
+		return GettextResource.gettext(catalog, msgid);
 	}
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(AuthServerAuthenticationProvider.class);
+	/**
+	 * Возвращает форму множественного числа для номера <VAR>n</VAR> перевода
+	 * переменной <VAR>msgid</VAR>.
+	 * 
+	 * @param catalog
+	 *            - экземпляр класса ResourceBundle
+	 * @param msgid
+	 *            - строка-ключ, которая должна быть переведена, используется
+	 *            ASCII-кодировка
+	 * @param msgid_plural
+	 *            - её форма множественного числа на английском языке
+	 * @return перевод переменной <VAR>msgid</VAR>, в зависимости от переменной
+	 *         <VAR>n</VAR>, или сама переменная <VAR>msgid</VAR>, или
+	 *         переменная <VAR>msgid_plural</VAR>, если перевод не найден
+	 */
+	public static String
+			ngettext(ResourceBundle catalog, String msgid, String msgid_plural, long n) {
+		return GettextResource.ngettext(catalog, msgid, msgid_plural, n);
+	}
 
-	private String innerMessage = null;
+	/**
+	 * Возвращает перевод переменной <VAR>msgid</VAR>, в зависимости от
+	 * контекстной переменной <VAR>msgctxt</VAR>.
+	 * 
+	 * @param catalog
+	 *            - экземпляр класса ResourceBundle
+	 * @param msgctxt
+	 *            - контекст для строки-ключа, используется ASCII-кодировка
+	 * @param msgid
+	 *            - строка-ключ, которая должна быть переведена, используется
+	 *            ASCII-кодировка
+	 * @return перевод переменной <VAR>msgid</VAR>, или сама переменная
+	 *         <VAR>msgid</VAR>, если перевод не найден
+	 */
+	public static String pgettext(ResourceBundle catalog, String msgctxt, String msgid) {
+		return GettextResource.pgettext(catalog, msgctxt, msgid);
+	}
 
-	@Override
-	public Authentication authenticate(final Authentication arg1) {
+	/**
+	 * * Возвращает форму множественного числа для номера <VAR>n</VAR> перевода
+	 * переменной <VAR>msgid</VAR>, в зависимости от контекстной переменной
+	 * <VAR>msgctxt</VAR>.
+	 * 
+	 * @param catalog
+	 *            - экземпляр класса ResourceBundle
+	 * @param msgctxt
+	 *            - контекст для строки-ключа, используется ASCII-кодировка
+	 * @param msgid
+	 *            - строка-ключ, которая должна быть переведена, используется
+	 *            ASCII-кодировка
+	 * @param msgid_plural
+	 *            - её форма множественного числа на английском языке
+	 * @return перевод переменной <VAR>msgid</VAR>, в зависимости от переменной
+	 *         <VAR>n</VAR>, или сама переменная <VAR>msgid</VAR>, или
+	 *         переменная <VAR>msgid_plural</VAR>, если перевод не найден
+	 */
+	public static String npgettext(ResourceBundle catalog, String msgctxt, String msgid,
+			String msgid_plural, long n) {
+		return GettextResource.npgettext(catalog, msgctxt, msgid, msgid_plural, n);
+	}
 
-		// TODO Auto-generated method stub
-		// arg0.getAuthorities().iterator().next().
-		// if (SecurityContextHolder.getContext().getAuthentication() != null) {
-		// if
-		// (SecurityContextHolder.getContext().getAuthentication().isAuthenticated())
-		// {
-		// return arg0;
+	/**
+	 * Метод установки ResourceBundle для дальнейшего использования в переводе
+	 * серверной части Showcase с помощью Gettext.
+	 * 
+	 * @return ResourceBundle
+	 */
+	public static ResourceBundle getLocalizedResourseBundle() {
+		// String lang = UserDataUtils.getLocaleForCurrentUserdata();
+
+		// ProcessBuilder pb = new ProcessBuilder();
+		// String classpath = pb.environment().get("CLASSPATH");
+		// String localizePath = classpath.substring(classpath.lastIndexOf(";")
+		// + 1);
+		// File localizeDir = new File(localizePath);
+
+		// String bundleFile = "";
+		// for (String file : localizeDir.list()) {
+		// if (file.equals(lang + ".class")) {
+		// bundleFile = file;
+		// break;
 		// }
 		// }
-		// authentication.
-		// UsernamePasswordAuthenticationToken arg0 = arg1;
 
-		String ipAddresOfRemouteHost =
-			((UserAndSessionDetails) arg1.getDetails()).getRemoteAddress();
+		File dir = UserDataUtils.getResourceDir(UserDataUtils.getUserDataId());
 
-		if (ipAddresOfRemouteHost == null) {
-			ipAddresOfRemouteHost = "";
-		}
-
-		String url = "";
-		String login = arg1.getPrincipal().toString();
-		String pwd = arg1.getCredentials().toString();
-
-		String sesid = ((UserAndSessionDetails) arg1.getDetails()).getSessionId();
-		// String oldSesid = AppInfoSingleton.getAppInfo().getSesid();
-		// String[] arr =
-		// AppInfoSingleton.getAppInfo().getRemoteAddrSessionMap().values().toArray(new
-		// String[0]);
-		// String oldSesid = arr.length > 0 ? arr[arr.length - 1] : "";
-		String oldSesid = "";
-		String remoteAddr = ((UserAndSessionDetails) arg1.getDetails()).getRemoteAddress();
-		if (remoteAddr != null)
-			oldSesid = AppInfoSingleton.getAppInfo().getRemoteAddrSessionMap().get(remoteAddr);
-		// if (sesid == null && "".equals(oldSesid)) {
-		// sesid = "RestfulSessId";
-		// oldSesid = "RestfulSessId";
-		// }
-
-		String groupProviders =
-			((UserAndSessionDetails) arg1.getDetails()).getUserInfo().getGroupProviders();
-
-		try {
-			url = SecurityParamsFactory.getLocalAuthServerUrl();
-		} catch (SettingsFileOpenException e1) {
-			throw new AuthenticationServiceException(SecurityParamsFactory.APP_PROP_READ_ERROR, e1);
-		}
-
-		// if ("9152046062107176349L_default_value".equals(pwd)) {
-
-		// для тестирования - надо удалить. начало.
-		if (AppInfoSingleton.getAppInfo().getSessionInfoMap().containsKey(oldSesid)
-				&& (AppInfoSingleton.getAppInfo().getSessionInfoMap().get(oldSesid)
-						.getAuthServerCrossAppPassword() != null)) {
-			if (AppInfoSingleton.getAppInfo().isEnableLogLevelInfo()) {
-				LOGGER.info("Попытка аутентификации сессии " + sesid
-						+ " через механизм кроссдоменной аутентификации. Пользователь: " + login
-						+ " Пароль: " + pwd);
+		String lang = "";
+		if (SecurityContextHolder.getContext().getAuthentication() != null) {
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			String sesid = ((WebAuthenticationDetails) auth.getDetails()).getSessionId();
+			if (sesid == null) {
+				String remoteAddr =
+					((WebAuthenticationDetails) auth.getDetails()).getRemoteAddress();
+				if (remoteAddr != null)
+					sesid =
+						AppInfoSingleton.getAppInfo().getRemoteAddrSessionMap().get(remoteAddr);
+				// sesid = AppInfoSingleton.getAppInfo().getSesid();
+				// String[] arr =
+				// AppInfoSingleton.getAppInfo().getRemoteAddrSessionMap().values()
+				// .toArray(new String[0]);
+				// if (arr.length > 0)
+				// sesid = arr[arr.length - 1];
 			}
+			lang = AppInfoSingleton.getAppInfo().getLocalizationCache().get(sesid);
 		}
 
-		// для тестирования - надо удалить. конец.
+		if (lang == null || "".equals(lang))
+			lang = UserDataUtils.getLocaleForCurrentUserdata(UserDataUtils.getUserDataId());
 
-		if (AppInfoSingleton.getAppInfo().getSessionInfoMap().containsKey(oldSesid)
-				&& (AppInfoSingleton.getAppInfo().getSessionInfoMap().get(oldSesid)
-						.getAuthServerCrossAppPassword() != null)
-				&& AppInfoSingleton.getAppInfo().getSessionInfoMap().get(oldSesid)
-						.getAuthServerCrossAppPassword().equals(pwd)) {
+		class MyLoader extends ClassLoader {
 
-			try {
-				// AppCurrContext.getInstance().setAuthViaAuthServ(true);
-				AppInfoSingleton.getAppInfo().setAuthViaAuthServerForSession(sesid, true);
-				((UserAndSessionDetails) arg1.getDetails()).setAuthViaAuthServer(true);
-
-				if (AuthServerUtils.getTheAuthServerAlias() == null) {
-					AuthServerUtils.init(url);
-				}
-
-				((UserAndSessionDetails) arg1.getDetails()).setUserInfo(AuthServerUtils
-						.getTheAuthServerAlias().isAuthenticated(oldSesid));
-
-			} finally {
-				AppInfoSingleton.getAppInfo().getSessionInfoMap().get(oldSesid)
-						.setAuthServerCrossAppPassword(null);
+			public MyLoader(ClassLoader parent) {
+				super(parent);
 			}
 
-		} else {
-			AuthServerUtils.init(url);
-
-			// UserData ud =
-			// AuthServerUtils.getTheAuthServerAlias().isAuthenticated(sesid);
-			// if (ud == null) {
-			try {
-				URL server;
-				if (groupProviders == null) {
-					server =
-						new URL(url
-								+ String.format("/login?sesid=%s&login=%s&pwd=%s&ip=%s", sesid,
-										encodeParam(login), encodeParam(pwd),
-										ipAddresOfRemouteHost));
-				} else {
-					server =
-						new URL(url
-								+ String.format("/login?sesid=%s&login=%s&pwd=%s&gp=%s&ip=%s",
-										sesid, encodeParam(login), encodeParam(pwd),
-										encodeParam(groupProviders), ipAddresOfRemouteHost));
-				}
-				HttpURLConnection c = null;
-				try {
-					c = (HttpURLConnection) server.openConnection();
-					c.setRequestMethod("GET");
-					c.connect();
-					// Thread.sleep(1000);
-					if (c.getResponseCode() == HttpURLConnection.HTTP_OK) {
-						// AppCurrContext.getInstance();
-						AppInfoSingleton.getAppInfo().setAuthViaAuthServerForSession(sesid, true);
-						((UserAndSessionDetails) arg1.getDetails()).setAuthViaAuthServer(true);
-						((UserAndSessionDetails) arg1.getDetails()).setUserInfo(AuthServerUtils
-								.getTheAuthServerAlias().isAuthenticated(sesid));
-
-						// AppCurrContext.getInstance().setAuthViaAuthServ(true);
-					} else {
-						if (AppInfoSingleton.getAppInfo().getIsCelestaInitialized()) {
-							AppInfoSingleton.getAppInfo().getCelestaInstance().failedLogin(login);
+			@Override
+			protected Class<?> findClass(String name) throws ClassNotFoundException {
+				File classFile = null;
+				if (dir.exists()) {
+					for (File file : dir.listFiles()) {
+						if (file.getName().substring(0, file.getName().lastIndexOf("."))
+								.equals(name)) {
+							classFile = file;
+							break;
 						}
-
-						String servletResponseMessage = "";
-						try {
-							servletResponseMessage = TextUtils.streamToString(c.getErrorStream());
-						} catch (Exception e) {
-						}
-
-						if (UserDataUtils
-								.getGeneralOptionalProp("mellophone.show.reason.for.blocked.user") != null
-								&& "true".equalsIgnoreCase(UserDataUtils.getGeneralOptionalProp(
-										"mellophone.show.reason.for.blocked.user").trim())) {
-							if (servletResponseMessage
-									.contains("locked out for too many unsuccessful login attempts")
-									&& servletResponseMessage.contains("Резюме:")) {
-								LOGGER.info("Пользователь " + login + " заблокирован меллофоном");
-								String time_to_unlock =
-									servletResponseMessage.substring(servletResponseMessage
-											.indexOf("Time to unlock"));
-								throw new BadCredentialsException("User '" + login
-										+ "' is blocked by mellophone. " + time_to_unlock);
-							}
-
-							if (servletResponseMessage
-									.contains("locked out for too many unsuccessful login attempts")
-									&& !servletResponseMessage.contains("Резюме:")) {
-								LOGGER.info("Пользователь " + login + " заблокирован меллофоном");
-								String time_to_unlock =
-									servletResponseMessage.substring(servletResponseMessage
-											.indexOf("Time to unlock"));
-								throw new BadCredentialsException("User '" + login
-										+ "' is already blocked by mellophone. " + time_to_unlock);
-							}
-						}
-
-						if (servletResponseMessage.contains("is blocked permanently")) {
-							LOGGER.info("Пользователь " + login
-									+ " заблокирован на постоянной основе");
-							throw new BadCredentialsException("User '" + login
-									+ "' is blocked by administrator");
-						}
-
-						if (servletResponseMessage.contains("Stored procedure message begin:")) {
-							int indexBegin =
-								servletResponseMessage.indexOf("Stored procedure message begin:");
-							int beginMessageLength = "Stored procedure message begin:".length();
-							int indexEnd =
-								servletResponseMessage.indexOf("Stored procedure message end.");
-
-							innerMessage =
-								servletResponseMessage.substring(indexBegin + beginMessageLength,
-										indexEnd).trim();
-
-							LOGGER.info(innerMessage);
-							throw new BadCredentialsException(innerMessage);
-						}
-
-						LOGGER.info("Пользователю " + login
-								+ " не удалось войти в систему: Bad credentials");
-						throw new BadCredentialsException("Bad credentials");
-
 					}
-				} finally {
-					if (c != null)
-						c.disconnect();
+				}
+				try {
+					byte[] b = loadData(classFile);
+					return defineClass(
+							classFile.getName().substring(0, classFile.getName().lastIndexOf(".")),
+							b, 0, b.length);
+				} catch (Exception e) {
+					return null;
 				}
 
-			} catch (BadCredentialsException | IllegalStateException | SecurityException
-					| IllegalFormatException | NullPointerException | IOException
-					| IndexOutOfBoundsException e) {
-
-				LOGGER.error("", e);
-
-				if ("Bad credentials".equals(e.getMessage())) {
-					throw new BadCredentialsException(e.getMessage(), e);
-				} else if (e.getMessage().contains("User")
-						&& e.getMessage().contains("is blocked by mellophone")) {
-					throw new BadCredentialsException(e.getMessage(), e);
-				} else if (e.getMessage().contains("User")
-						&& e.getMessage().contains("is already blocked by mellophone")) {
-					throw new BadCredentialsException(e.getMessage(), e);
-				} else if (e.getMessage().contains("User")
-						&& e.getMessage().contains("is blocked by administrator")) {
-					throw new BadCredentialsException(e.getMessage(), e);
-				} else if (innerMessage != null && e.getMessage().contains(innerMessage)) {
-					throw new BadCredentialsException(e.getMessage(), e);
-				} else {
-					throw new BadCredentialsException("Authentication server is not available: "
-							+ e.getMessage(), e);
-				}
-			} catch (Exception err) {
-				if (AppInfoSingleton.getAppInfo().isEnableLogLevelError()) {
-					LOGGER.error("Ошибка фиксации неудачного логина в коде celesta", err);
-				}
 			}
-			// }
+
+			@Override
+			protected void finalize() throws Throwable {
+				super.finalize();
+			}
 		}
 
-		// привязки сессии приложения к пользователю celesta
-		// if (AppInfoSingleton.getAppInfo().getIsCelestaInitialized()) {
+		ResourceBundle rb = null;
 		try {
-			AppInfoSingleton
-					.getAppInfo()
-					.getCelestaInstance()
-					.login(sesid,
-							((UserAndSessionDetails) arg1.getDetails()).getUserInfo().getSid());
-			AppInfoSingleton
-					.getAppInfo()
-					.getSessionSidsMap()
-					.put(sesid, ((UserAndSessionDetails) arg1.getDetails()).getUserInfo().getSid());
-		} catch (Exception e) {
-			if (AppInfoSingleton.getAppInfo().isEnableLogLevelError()) {
-				LOGGER.error("Ошибка привязки сессии приложения к пользователю в celesta", e);
+			Locale.setDefault(new Locale(lang));
+			rb =
+				ResourceBundle.getBundle("loc", new Locale(lang), new MyLoader(Thread
+						.currentThread().getContextClassLoader()));
+			Runtime.getRuntime().runFinalization();
+		} catch (Throwable e) {
+			rb = null;
+		}
+		return rb;
+	}
+
+	/**
+	 * Метод установки ResourceBundle для дальнейшего использования в переводе
+	 * серверной части Showcase с помощью Gettext с учётом передачи языка в этот
+	 * метод.
+	 * 
+	 * @param lang
+	 *            - передаваемый извне язык
+	 * @return ResourceBundle
+	 */
+	public static ResourceBundle getLocalizedResourseBundle(String lang) {
+		File dir = UserDataUtils.getResourceDir(UserDataUtils.getUserDataId());
+
+		class MyLoader extends ClassLoader {
+
+			public MyLoader(ClassLoader parent) {
+				super(parent);
+			}
+
+			@Override
+			protected Class<?> findClass(String name) throws ClassNotFoundException {
+				File classFile = null;
+				if (dir.exists()) {
+					for (File file : dir.listFiles()) {
+						if (file.getName().substring(0, file.getName().lastIndexOf("."))
+								.equals(name)) {
+							classFile = file;
+							break;
+						}
+					}
+				}
+				try {
+					byte[] b = loadData(classFile);
+					return defineClass(
+							classFile.getName().substring(0, classFile.getName().lastIndexOf(".")),
+							b, 0, b.length);
+				} catch (Exception e) {
+					return null;
+				}
+
+			}
+
+			@Override
+			protected void finalize() throws Throwable {
+				super.finalize();
 			}
 		}
-		// }
 
-		// Authentication g = new Authentication.;
-
-		// try {
-		// arg0.setAuthenticated(true);
-		// } catch (Exception e) {
-		// }
-		// SecurityContextHolder.getContext().setAuthentication(arg0);
-
-		return arg1;
-
+		ResourceBundle rb = null;
+		try {
+			Locale.setDefault(new Locale(lang));
+			rb =
+				ResourceBundle.getBundle("loc", new Locale(lang), new MyLoader(Thread
+						.currentThread().getContextClassLoader()));
+			Runtime.getRuntime().runFinalization();
+		} catch (Throwable e) {
+			rb = null;
+		}
+		return rb;
 	}
 
-	@Override
-	public boolean supports(final Class<? extends Object> arg0) {
-		return SignedUsernamePasswordAuthenticationToken.class.isAssignableFrom(arg0);
-	}
+	/**
+	 * Вспомогательный метод, используемый при загрузке классов.
+	 */
+	private static byte[] loadData(File file) throws Exception {
+		int maxBufferSize = 1 * 1024 * 1024;
+		byte[] buffer = new byte[maxBufferSize];
 
-	public static String encodeParam(final String param) throws UnsupportedEncodingException {
-		String s = param;
-		s = s.replace("%", "AB4AFD63A4C");
-		s = s.replace("+", "D195B4C989F");
-		s = URLEncoder.encode(s, "ISO8859_1");
-		return s;
-	}
+		try (FileInputStream fileInputStream = new FileInputStream(file);) {
+			int bytesAvailable = fileInputStream.available();
+			int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+			buffer = new byte[bufferSize];
 
+			int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+			while (bytesRead > 0) {
+				bytesAvailable = fileInputStream.available();
+				bufferSize = Math.min(bytesAvailable, maxBufferSize);
+				bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+			}
+		}
+		return buffer;
+	}
 }
